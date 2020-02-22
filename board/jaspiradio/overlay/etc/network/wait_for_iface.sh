96203407ec2ac6f14d924bfac9c444598bcd0986
Jasper van Bourgognie <van.bourgognie@gmail.com>
2020-02-22 21:30:14 +0100

    use open network for fast connection

---
+++ board/jaspiradio/overlay/etc/network/wait_for_iface.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ATTEMPTS=100
+while [ ! -f /sys/class/net/$1/dev_id ] && [ "$ATTEMPTS" -gt "0" ]; do
+  ATTEMPTS=$(( $ATTEMPTS - 1 ))
+done
+
+if [ "$ATTEMPTS" -eq "0" ]; then
+  echo failed to find $1
+else
+  echo $1 showed up
+fi
